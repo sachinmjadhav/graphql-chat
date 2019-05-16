4b11f553d7f22f823f5e80f125a4e11bac03c64e
sachinmjadhav <sachin0321@gmail.com>
2019-05-16 14:10:35 +0530

    removed style comments

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,43 +1,6 @@
 import React, { useState } from 'react';
 import './Signup.css';
 
-// const style = {
-//   wrapper: {
-//     display: 'flex',
-//     flexDirection: 'column',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     padding: 5
-//   },
-//   label: {
-//     fontSize: 24,
-//     fontWeight: 'bolder',
-//     padding: 10,
-//     margin: '0 0 5px 0'
-//   },
-//   input: {
-//     backgroundColor: '#FFFFFF',
-//     border: '1px solid #ddd',
-//     padding: 7,
-//     margin: 0,
-//     height: 30,
-//     borderRadius: 10,
-//     outline: 'none',
-//     fontSize: 16,
-//     width: 200
-//   },
-//   button: {
-//     backgroundColor: '#6fb0dc',
-//     fontSize: 16,
-//     outline: 'none',
-//     border: '1px solid #ddd',
-//     padding: 17,
-//     margin: '0 0 0 5px',
-//     borderRadius: 10,
-//     color: '#fff'
-//   }
-// };
-
 const Signup = ({ signin }) => {
   const [username, setUsername] = useState('');
 
